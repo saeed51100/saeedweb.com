e3d0e99edd1a3d2605b5ab8a08cc216b20e8b587
saeed <saeed@saeed.com>
2022-03-11 21:05:09 +0330

    khoone1111

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -2,9 +2,7 @@
 
     <div class="row container bg-info" style="margin-top: 58px">
 
-
 		<?php get_template_part( 'sidebarloop' ); ?>
-
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 <!--bg-light--> bg-warning">
 
 			<?php if ( have_posts() ) : ?>
