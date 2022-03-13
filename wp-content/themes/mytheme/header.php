986ac82c6e39221859fadef5d272dd879901832c
saeed <saeed@saeed.com>
2022-03-13 18:05:03 +0330

    khooneh222

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -56,7 +56,7 @@
 
 
         <!--    saeed: search form using ivory search shortcode.  -->
-		<?php echo do_shortcode( '[ivory-search id="57" title="Default Search Form"]' ); ?>
+		<?php echo do_shortcode( '[ivory-search id="201" title="Default Search Form"]' ); ?>
 
 
         <nav class="nav main-nav">
