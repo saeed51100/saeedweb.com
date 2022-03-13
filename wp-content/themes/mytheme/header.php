96310550be3d9414d23ad0194748e928e4a39046
saeed <saeed@saeed.com>
2022-03-13 14:16:40 +0330

    edarenew1

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -1,28 +1,31 @@
 <!doctype html>
 <html <?php language_attributes(); ?> dir="rtl">
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
-    <title><?php bloginfo( 'name' ); ?></title>
+    <title><?php bloginfo('name'); ?></title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/dashboard/">
 
 
+    <!-- https://www.webstand.ir/add-css-and-js-file-to-theme-with-wp-enqueue/ -->
+    <!--    saeed: replace link below css & js -->
+
     <!-- Bootstrap core CSS -->
-    <link href="<?php bloginfo( 'template_url' ); ?>/css/bootstrap.rtl.css" rel="stylesheet">
+    <link href="<?php bloginfo('template_url'); ?>/css/bootstrap.rtl.css" rel="stylesheet">
 
     <!-- style.css required for wordpress -->
-    <link href="<?php bloginfo( 'stylesheet_url' ); ?>" rel="stylesheet">
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
 
     <style>
 
 
     </style>
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 
 </head>
 <!--<body>-->
@@ -31,11 +34,11 @@
 <header class="navbar navbar-expand-md navbar-dark bg-danger fixed-top shadow">
     <div class="container">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">
-			<?php if ( has_custom_logo() ) : ?>
-				<?php the_custom_logo(); ?>
-			<?php else : ?>
-                <h5><?php bloginfo( 'name' ); ?></h5>
-			<?php endif; ?>
+            <?php if (has_custom_logo()) : ?>
+                <?php the_custom_logo(); ?>
+            <?php else : ?>
+                <h5><?php bloginfo('name'); ?></h5>
+            <?php endif; ?>
         </a>
 
         <button class="navbar-toggler position-absolute d-md-none collapsed"
@@ -50,7 +53,7 @@
         <!--
         saeed: search form without widget.
         <div class="m-2">
-            <form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+            <form method="get" action="<?php echo esc_url(home_url('/')); ?>">
                 <input name="s" class="form-control" type="search" placeholder="Search" aria-label="Search">
             </form>
         </div>
@@ -64,29 +67,29 @@
 
 
         <!--    saeed: search form using ivory search shortcode.  -->
-		<?php echo do_shortcode( '[ivory-search id="57" title="Default Search Form"]' ); ?>
+        <?php echo do_shortcode('[ivory-search id="57" title="Default Search Form"]'); ?>
 
 
         <nav class="nav main-nav">
             <div class="container">
                 <!--            https://github.com/wp-bootstrap/wp-bootstrap-navwalker -->
-				<?php
-				$args = array(
-					'theme_location'  => 'primary',
-					'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
-					'container'       => 'div',
-					'container_class' => 'collapse navbar-collapse',
-					'container_id'    => 'bs-example-navbar-collapse-1',
+                <?php
+                $args = array(
+                    'theme_location' => 'primary',
+                    'depth' => 2, // 1 = no dropdowns, 2 = with dropdowns.
+                    'container' => 'div',
+                    'container_class' => 'collapse navbar-collapse',
+                    'container_id' => 'bs-example-navbar-collapse-1',
 //				'menu_class'      => 'navbar-nav mr-auto',
-					'menu_class'      => 'nav nav-pills pull-right',
-					'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-					'walker'          => new WP_Bootstrap_Navwalker(),
+                    'menu_class' => 'nav nav-pills pull-right',
+                    'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker' => new WP_Bootstrap_Navwalker(),
 
 
-				);
-				?>
+                );
+                ?>
 
-				<?php wp_nav_menu( $args ); ?>
+                <?php wp_nav_menu($args); ?>
             </div>
         </nav>
     </div>
