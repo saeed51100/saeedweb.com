04e4d4d9b59020b2dea45bb8d776147df63db0f1
saeed <saeed@saeed.com>
2022-03-13 12:19:39 +0330

    edarenew1

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -38,7 +38,9 @@
 			<?php endif; ?>
         </a>
 
-
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <!--
         saeed: search form without widget.
         <div class="m-2">
