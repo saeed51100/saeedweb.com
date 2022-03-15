8ed4416d3adb1333901d90a122b9d61d5e964adf
saeed <saeed@saeed.com>
2022-03-15 20:53:10 +0330

    khoone3111

--- wp-content/themes/mytheme/sidebarloop.php
+++ wp-content/themes/mytheme/sidebarloop.php
@@ -1,4 +1,4 @@
-<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-success sidebar collapse">
+<nav id="sidebarMenu" class="col-md-3 col-lg-3 d-md-block bg-success sidebar collapse">
 
 
     <!--
