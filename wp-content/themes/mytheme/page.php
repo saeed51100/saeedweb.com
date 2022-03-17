0b2fff4c04320eea1b564844cb26c7fe8a476e00
saeed <saeed@saeed.com>
2022-03-17 23:58:31 +0330

    khoone3111

--- wp-content/themes/mytheme/page.php
+++ wp-content/themes/mytheme/page.php
@@ -1,43 +1,43 @@
 <?php get_header(); ?>
 
-    <div class="row container" style="margin-top: 58px">
-
-
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-light">
-            <div class="container">
-				<?php if ( have_posts() ) : ?>
-				<?php while ( have_posts() ) :
-				the_post(); ?>
-                <section class="row title-bar">
-                    <div class="container">
-                        <div class="col-md-12">
-                            <h1><?php the_title(); ?></h1>
+    <div class="container">
+        <div class="row" style="margin-top: 58px">
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-light">
+                <div class="container">
+                    <?php if (have_posts()) : ?>
+                    <?php while (have_posts()) :
+                    the_post(); ?>
+                    <section class="row title-bar">
+                        <div class="container">
+                            <div class="col-md-12">
+                                <h1><?php the_title(); ?></h1>
+                            </div>
                         </div>
-                    </div>
-                </section>
+                    </section>
 
-                <section class="row main">
-                    <div class="container">
-                        <div class="col-md-12">
+                    <section class="row main">
+                        <div class="container">
+                            <div class="col-md-12">
 
-                            <article class="page">
-                                <div class="post-thumbnail">
-									<?php if ( has_post_thumbnail() ): ?>
-										<?php the_post_thumbnail(); ?>
-									<?php endif; ?>
-                                </div>
-                                <br>
-								<?php the_content(); ?>
-                            </article>
-                            <div class="clr"></div>
-							<?php endwhile; ?>
-							<?php endif; ?>
+                                <article class="page">
+                                    <div class="post-thumbnail">
+                                        <?php if (has_post_thumbnail()): ?>
+                                            <?php the_post_thumbnail(); ?>
+                                        <?php endif; ?>
+                                    </div>
+                                    <br>
+                                    <?php the_content(); ?>
+                                </article>
+                                <div class="clr"></div>
+                                <?php endwhile; ?>
+                                <?php endif; ?>
 
-                </section>
+                    </section>
 
 
-            </div>
-        </main>
+                </div>
+            </main>
+        </div>
     </div>
 
 
