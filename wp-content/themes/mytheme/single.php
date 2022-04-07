69937a8e59f3af412f32db297cec923b856a7eaf
saeed <saeed@saeed.com>
2022-04-07 22:49:31 +0430

    khooneh 1401/01/17-4

--- wp-content/themes/mytheme/single.php
+++ wp-content/themes/mytheme/single.php
@@ -98,8 +98,8 @@
 							<?php
 
 							if (
-								( is_single() || is_page() )
-								&& ( comments_open() || get_comments_number() )
+								//	( is_singular() || is_page() ) &&
+								( comments_open() || get_comments_number() )
 								&& ! post_password_required()
 							) {
 								?>
