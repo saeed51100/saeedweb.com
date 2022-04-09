27332eb445bf4b345002420030817546388cad1a
saeed <saeed@saeed.com>
2022-04-09 22:19:22 +0430

    khooneh 1401/01/20-2

--- wp-content/themes/mytheme/template-parts/all-post-title.php
+++ wp-content/themes/mytheme/template-parts/all-post-title.php
@@ -4,27 +4,34 @@
 	-->
 
 <ul class="btn-toggle-nav list-unstyled scrollarea mx-2 bg-light rounded shadow">
-	<?php
-	$current_id = get_the_ID();
-	$args       = array(
-		'post_type'      => 'post',
-		'orderby'        => 'ID',
-		'post_status'    => 'publish',
-		'order'          => 'DESC',
-		'posts_per_page' => - 1 // this will retrive all the post that is published
-	);
-	$result     = new WP_Query( $args );
-	if ( $result->have_posts() ) :
-		?>
-		<?php while ( $result->have_posts() ) : $result->the_post(); ?>
-		<li>
-			<a href="<?php the_permalink(); ?>"
-			   class="link-dark rounded
+    <div class="side-widget m-3" >
+        <h3>عنوان پست</h3>
+        <hr>
+    </div>
+    <div class="m-2">
+	    <?php
+	    $current_id = get_the_ID();
+	    $args       = array(
+		    'post_type'      => 'post',
+		    'orderby'        => 'ID',
+		    'post_status'    => 'publish',
+		    'order'          => 'DESC',
+		    'posts_per_page' => - 1 // this will retrive all the post that is published
+	    );
+	    $result     = new WP_Query( $args );
+	    if ( $result->have_posts() ) :
+		    ?>
+		    <?php while ( $result->have_posts() ) : $result->the_post(); ?>
+            <li>
+                <a href="<?php the_permalink(); ?>"
+                   class="link-dark rounded
     	<?php echo( $post->ID == $current_id && is_single() ? 'list-group-item' : '' ); ?>">
-				<?php the_title(); ?>
-			</a>
-		</li>
-	<?php endwhile; ?>
-	<?php endif; ?>
-	<?php wp_reset_postdata(); ?>
+				    <?php the_title(); ?>
+                </a>
+            </li>
+	    <?php endwhile; ?>
+	    <?php endif; ?>
+	    <?php wp_reset_postdata(); ?>
+    </div>
+
 </ul>
