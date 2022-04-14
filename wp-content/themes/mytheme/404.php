68ac75fc188b5afe456dc6e9e84b9243d5b43aef
saeed <saeed@saeed.com>
2022-04-14 20:43:35 +0430

    khooneh 1401/01/24-1

---
+++ wp-content/themes/mytheme/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div style="background-color: #f3edde">
+    <div class="container">
+        <div class="row" style="margin-top: 38px">
+
+			<?php get_sidebar(); ?>
+            <main class="col-md-9 col-lg-9">
+                <h1 class="st404">صفحه مورد نظر پیدا نشد</h1>
+            </main>
+
+        </div>
+    </div>
+
+</div>
+<?php get_footer(); ?>
